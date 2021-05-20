3b96908cbf102c0949d3cb417f7a644b5e2a8be5
OnishenkoEgor <egoronishenko@yandex.ru>
2021-05-21 01:47:52 +0300

    main-preview with autoplay

--- src/components/main-preview-list/main-preview-list.js
+++ src/components/main-preview-list/main-preview-list.js
@@ -4,15 +4,17 @@ class MainPreviewList extends Component {
 
     state = {
         menu: [],
-        active: 0
+        active: 0,
+        autoplay:null
+    }
+    componentDidMount(){
+        this.setState({autoplay:setInterval(()=>this.nextSlide(),5000)})
     }
     componentDidUpdate(prevProps) {
         if (prevProps.menu !== this.props.menu && this.props.menu.length>0) {
             this.setState({ menu: this.menuToggler(this.props.menu)},console.debug(this.state.menu))
         }
-        
     }
-
     menuToggler = (menu) => {
         return menu.map((item, index) => {
             return index === this.state.active ? { ...item, active: true } : { ...item, active: false }
@@ -23,16 +25,17 @@ class MainPreviewList extends Component {
         this.setState(({active,menu }) => ({
             active: active>0? --active:menu.length-1,
             menu : this.menuToggler(menu)
-
         }))
-        
+        clearInterval(this.state.autoplay)
+        this.setState({autoplay:setInterval(()=>this.nextSlide(),5000)})
     }
     nextSlide = () => {
         this.setState(({ active,menu }) => ({
             active: active<menu.length-1?++active:0,
             menu : this.menuToggler(menu)
         }))
-        
+        clearInterval(this.state.autoplay)
+        this.setState({autoplay:setInterval(()=>this.nextSlide(),5000)})
     }
     render() {
         return (
